b1491506212927a38e5862c8ecb1c0951a54fe8b
Mehmet Büyükgümüş <62846517+mehmetbuyukgumus@users.noreply.github.com>
2024-02-14 14:02:07 +0100

    Recommendation Systems Module

---
+++ Recommendation Systems/association_rule_learning.py
@@ -0,0 +1,14 @@
+############################################
+# ASSOCIATION RULE LEARNING (BİRLİKTELİK KURALI ÖĞRENİMİ)
+############################################
+
+# 1. Veri Ön İşleme
+# 2. ARL Veri Yapısını Hazırlama (Invoice-Product Matrix)
+# 3. Birliktelik Kurallarının Çıkarılması
+# 4. Çalışmanın Scriptini Hazırlama
+# 5. Sepet Aşamasındaki Kullanıcılara Ürün Önerisinde Bulunmak
+
+############################################
+# 1. Veri Ön İşleme
+############################################
+
